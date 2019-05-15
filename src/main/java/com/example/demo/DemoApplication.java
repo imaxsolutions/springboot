67a40e14f846fada2cfa1138abc13ae19b53d559
Sankar Singh <sankar.singu@gmail.com>
2019-05-15 21:12:45 +0530

    commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,7 +2,7 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//qa-1002
 @SpringBootApplication
 public class DemoApplication {
 
